cc3ff1b55ec89c55c7383be21f291b1169c9adf8
rdbruijne <rickb1988@gmail.com>
2019-08-08 19:54:22 +0200

    Inital commit

---
+++ CopyFullPath/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Principal;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CopyFullPath
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 0)
+			{
+				foreach (string a in args)
+				{
+					if (a.ToLower() == "-install")
+					{
+						string cmd = "\\shell\\Copy full path\\command";
+						string cmdVal = $"{System.Reflection.Assembly.GetExecutingAssembly().Location} \"%1\"";
+						if(new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator)) // is admin
+						{
+							foreach(string key in new string[]{ "Directory\\Background", "Directory", "*" })
+							{
+								Microsoft.Win32.RegistryKey k = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey(key + cmd);
+								k.SetValue("", cmdVal);
+								k.Close();
+							}
+						}
+						else
+						{
+							foreach(string key in new string[]{ "Software\\Classes\\directory\\Background", "Software\\Classes\\directory", "Software\\Classes\\*" })
+							{
+								Microsoft.Win32.RegistryKey k = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(key + cmd);
+								k.SetValue("", cmdVal);
+								k.Close();
+							}
+						}
+						return;
+					}
+				}
+
+				Thread thread = new Thread(() => Clipboard.SetText(System.IO.Path.GetFullPath(args[0]).Replace('\\', '/')));
+				thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+				thread.Start();
+				thread.Join();
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CopyFullPath
+
 Adds context menu option to copy the path of a file or directory to the clipboard.
+Install using `CopyFullPath.exe -install`.
